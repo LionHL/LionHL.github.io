4548092ba2f27a17f236ed3ea354997cfec85bdb
helu <190195211@qq.com>
2017-06-30 13:35:39 +0800

    test commit

--- themes/yalia/source-src/js/main.js
+++ themes/yalia/source-src/js/main.js
@@ -1,29 +1,15 @@
-require('./jquery')
+// 样式
+import '../css/main.scss'
+// 上报
+import './report'
+// 图片查看器
+import Viewer from './viewer'
+// 分享
+import Share from './share'
 
-var tags = require('./tags')
-var archiveInner = require('./archive-inner')
-var tools = require('./tools')
-var browser = require('./browser')
-var fixPage = require('./fix-page')
-var mobile = require('./mobile')
-var viewer = require('./viewer')
+import {addLoadEvent} from './util'
 
-$(function() {
-	viewer.init()
-	archiveInner.init()
-	fixPage.init()
-	tags.init()
-	// todo: resize destrop
-	if(browser.versions.mobile === true && $(window).width() < 800){
-		mobile.init()
-	}else{
-		tools.init()
-		$('.js-smart-menu').click(function(e) {
-			e.stopPropagation()
-			tools.show($(this).data('idx'))
-		})
-		$('.left-col,.mid-col').click(function() {
-			tools.hide()
-		})
-	}
+addLoadEvent(function() {
+	Share.init()
+	Viewer.init()
 })
